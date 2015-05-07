7f11fa53eb96b247f6b2cae66445334bc6063e74
HelloAsp_net <370402022@qq.com>
2015-05-08 02:54:44 +0800

    创建项目

---
+++ Template/struct.js
@@ -0,0 +1,22 @@
+/**
+ * Created by xyz on 2015/5/8.
+ */
+var struct = {
+
+  't-object': '基类',
+
+  // 元件
+  't-unit': '元件类',
+  't-unit-drag': '元件类-拖动组件',
+  't-unit-drag-container': '元件类-拖动组件容器',
+
+
+  // 实体件
+  't-entity': '实体类',
+  't-entity-toolbar': '实体类-工具栏',
+  't-entity-win': '实体类-弹出窗',
+
+
+
+  't-other': '其他'
+};
