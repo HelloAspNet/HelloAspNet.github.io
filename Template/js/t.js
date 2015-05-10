ee6ee789ff7a12c70fbe2710365ee2356aeb7bff
HelloAsp_net <370402022@qq.com>
2015-05-11 01:14:44 +0800

    修改 2015-05-08

--- Template/js/t.js
+++ Template/js/t.js
@@ -2,9 +2,9 @@
  * Created by xyz on 2015/5/8.
  */
 var Maps = {
-  'test-unit-1': '<div style="color: #f00;background-color: #ddd;">测试元件1-html内容</div>',
-  'test-unit-2': '<div style="color: #00f;background-color: #ddd;">测试元件2-html内容</div>',
-  'test-unit-3': '<div style="color: #0f0;background-color: #ddd;">测试元件3-html内容</div>',
+  'test-unit-1': '<div style="color: #f00;">测试元件1-html内容</div>',
+  'test-unit-2': '<div style="color: #00f;">测试元件2-html内容</div>',
+  'test-unit-3': '<div style="color: #0f0;">测试元件3-html内容</div>',
   'test-other': '<i>other</i>'
 };
 
@@ -13,6 +13,7 @@ $(function(){
   var $win = $(window);
   var $doc = $(document);
   var $new_container = $('.t-new-container');
+  var $active_region = $('.t-active-region');
 
   // $active的offset
   var Offset = { x: 0, y: 0 };
@@ -22,26 +23,58 @@ $(function(){
       // 鼠标弹起时清空新建的实体
       'mouseup': function(){
         $('.t-new').removeClass('t-new');
-        $('.t-active').removeClass('t-active');
+        $('.t-state-active').removeClass('t-state-active');
         //$new_container.html('');
       },
       // 鼠标移动时新建实体跟着移动
       'mousemove': function(e){
-        var $active = $('.t-active.t-dragable');
+        var $active = $('.t-state-active.t-dragable');
         //if(e.target === $active[0]) {
           var isNew = $active.hasClass('t-new');
+          var size = {
+            width: $active.width(),
+            height: $active.height()
+          };
+          var position = {};
           if (isNew) {
-            $active.css({
+            position = {
               top: e.clientY,
               left: e.clientX
-            });
+            };
           }
           else {
-            $active.css({
+            position = {
               top: e.clientY - Offset.y,
               left: e.clientX - Offset.x
-            });
+            };
           }
+
+        $active.css(position);
+          $('.t-unit-container').each(function () {
+            var $this = $(this);
+
+            var top = parseInt($this.offset().top);
+            var left = parseInt($this.offset().left);
+            var width = parseInt($this.css('width'));
+            var height = parseInt($this.css('height'));
+
+            if (position.top > top && position.top < top + height && position.left > left && position.left < left + width) {
+              $active_region.addClass('t-state-active').animate({
+                top: top,
+                left: left,
+                width: size.width,
+                height: size.height
+              });
+            }
+            else {
+//              $active_region.removeClass('t-state-active').stop(true).animate({
+//                top: 0,
+//                left: 0,
+//                width: 0,
+//                height: 0
+//              });
+            }
+          });
         //}
       }
     })
@@ -52,7 +85,7 @@ $(function(){
         var uid = $this.closest('.t-unit-drag').data('uid');
         var $template = $(Maps[uid]);
         $template
-          .addClass('t-new t-active t-dragable')
+          .addClass('t-new t-state-active t-dragable')
           .css({
             top: e.clientY,
             left: e.clientX
@@ -70,7 +103,7 @@ $(function(){
           y: e.offsetY
         };
         $this
-          .addClass('t-active')
+          .addClass('t-state-active')
           .css({
             top: e.clientY - Offset.y,
             left: e.clientX - Offset.x
